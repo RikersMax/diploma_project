83b8cd8a766ca7164528445b120d0939e3965db3
RikersMax <larionov01996@gmail.com>
2025-05-06 19:32:55 +0500

    test seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+=begin
+=end
+u = User.new(user_name: 'test', email: 'test@email.com')
+u.password = 'test'
+u.password_confirmation = 'test'
+u.save
+u.remember_me
+
+TypeObject.create(name_type: 'expense')
+TypeObject.create(name_type: 'income')
+
+KindOfObject.create(name_kind: 'personal_object')
+KindOfObject.create(name_kind: 'shared_object')
+
+
+AccountingObject.create(name_object: 'test_object', user_id: u.id, type_object_id: 1, kind_of_object_id: 1)
+
+
+
+
